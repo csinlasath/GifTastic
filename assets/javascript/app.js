7fa6d591b37d8d3824d36b2aa213778b305306fb
Christopher Sinlasath <csinlasath@live.com>
2018-12-26 18:30:59 -0600

    Initial Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var favoriteGifs = [];
+var gifTopics = [];
+
+var giphyAPIKey = "RFgjUvZdnGSci14BZRCksEu9kKe7P8HK";
+
+/*
+Add 10 Gifs per page
+Create favorite section
+create topic section
+additional meta tags
+responsive
+one click download
+
+under every gif, show rating
+
+loop and append buttons
+non animated, animate when clicked
+
+add a form for user input and add to array for topics
+
+*/
